a007cf316fd1808cc1f07e278bf5ba10e5de0c7a
Gael Jay <gael.jay@proton.me>
2026-02-06 08:12:41 +0100

    test: add buttons

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,18 @@
-document.querySelectorAll(".artwork");
+const buttons = document.querySelectorAll(".filters button");
+const artworks = document.querySelectorAll(".artwork");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const filter = button.dataset.filter;
+
+    artworks.forEach(art => {
+      const tags = art.dataset.tags.split(" ");
+
+      if (filter === "all" || tags.includes(filter)) {
+        art.style.display = "block";
+      } else {
+        art.style.display = "none";
+      }
+    });
+  });
+});
